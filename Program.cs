4b600d8c20da5008a284ca48561a9cf472b0ec8e
Morales1429 <99547748+Morales1429@users.noreply.github.com>
2022-08-21 17:42:56 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Ingrese su puntuacion con coma");
+double puntuacion = double.Parse(Console.ReadLine());
+double bonificacion = 50000;
+double total = puntuacion*bonificacion;
+Console.WriteLine("Su nivel de evaluacion es "+puntuacion+" su bonificacion es "+total);
